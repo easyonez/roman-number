ccfc24d0627f4c293ee0b9338d9b716c50b0976c
easyonez <aala@ajmai.me>
2026-05-09 16:22:33 +0200

    #6 implementa conversione primi 6 numeri

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -8,9 +8,19 @@ package it.unipd.mtss;
 public class IntegerToRoman {
     public static String convert(int number) {
         StringBuilder result = new StringBuilder();
+
         for (int i = 0; i < number; i++) {
-            result.append("I");
+            if ((i + 1) % 5 == 4) {
+                result.delete(result.length() - 3, result.length());
+                result.append("IV");
+            } else if ((i + 1) % 5 == 0) {
+                result.delete(result.length() - 2, result.length());
+                result.append("V");
+            } else {
+                result.append("I");
+            }
         }
+
         return result.toString();
     }
 }
